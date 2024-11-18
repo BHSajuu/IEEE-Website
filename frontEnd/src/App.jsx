353a45db0939c27d954ba47fa3520d55e4d48f92
Boktiar Hussain <bokthairhussain@gmail.com>
2024-11-18 23:26:02 +0530

    first commit

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>
+        Let's start building the website! Share your thoughts, and we can plan
+        the next steps.
+      </h1>
+    </>
+  );
+}
+
+export default App;
